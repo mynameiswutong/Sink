a0f50f3e6812a7875f60aa0e6d119d0915f3c2c0
Jans <1375369513@qq.com>
2026-02-28 12:23:42 +0800

    feat(tabs): 为标签页添加液态滑动指示器效果

--- layers/dashboard/app/components/dashboard/links/QRCode.vue
+++ layers/dashboard/app/components/dashboard/links/QRCode.vue
@@ -155,15 +155,3 @@ onMounted(() => {
     </div>
   </div>
 </template>
-
-<style scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 3s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
